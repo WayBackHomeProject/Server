66ddd97a17263409dae2d6b7f8f6a668886d08df
limnyn <hogandi001@gmail.com>
2024-05-20 15:10:14 +0900

    feat : [API 추가] GET /policestation_coordinates_in_radius/

--- wbh_api_server/data_api/views.py
+++ wbh_api_server/data_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import CCTV, ConvenienceStore
+from .models import CCTV, ConvenienceStore, PoliceStation
 import math
 
 @api_view(['GET'])
@@ -93,7 +93,6 @@ def conveniencestore_coordinates_in_radius(request):
 
     # Retrieve all CCTV records
     convenience_stores = ConvenienceStore.objects.all()
-    print(convenience_stores[0])
     results = []
 
     # Filter CCTV coordinates within the radius
@@ -114,6 +113,47 @@ def conveniencestore_coordinates_in_radius(request):
             })
 
     return Response(results)
+
+
+@api_view(['GET'])
+def policestation_coordinates_in_radius(request):
+    """
+    현재 좌표(lat, lng)을 기반으로 반경 5km 내의 cctv 좌표를 
+    ConvenienceStore모델(data_api_convenience_store 테이블)에서 찾아서 조건에 일치하는 레이블들을
+    json 형식으로 반환하는 함수
+    """
+    try:
+        # Get latitude and longitude from request parameters
+        lat = float(request.GET.get('lat'))
+        lng = float(request.GET.get('lng'))
+        radius_km = float(request.GET.get('radius_km'))
+    except (TypeError, ValueError):
+        return Response({'error': 'Invalid or missing lat/lng parameters'}, status=400)
+    
+    
+
+    # Retrieve all CCTV records
+    police_stations = PoliceStation.objects.all()
+    results = []
+
+    # Filter CCTV coordinates within the radius
+    for station in police_stations:
+        distance = haversine(lat, lng, station.latitude, station.longitude)
+        if distance <= radius_km:
+            results.append({
+                'number': station.type,
+                'sido' : station.sido,
+                'policestation': station.policestation,
+                'department': station.department,
+                'type' : station.type,
+                'tel_no': station.tel_no,
+                'address' : station.address,
+                'latitude': station.latitude,
+                'longitude': station.longitude,
+            })
+
+    return Response(results)
+
 
 
 
